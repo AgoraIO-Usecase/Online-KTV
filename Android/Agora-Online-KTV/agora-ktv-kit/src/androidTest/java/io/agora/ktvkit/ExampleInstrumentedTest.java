b5a2c5eee08de38911b8538c29d53215b96f3a55
Hai Guo <deliberatekids@gmail.com>
2018-09-19 19:28:41 +0800

    fix packaging name wrong

--- Android/Agora-Online-KTV/agora-ktv-kit/src/androidTest/java/io/agora/ktvkit/ExampleInstrumentedTest.java
+++ Android/Agora-Online-KTV/agora-ktv-kit/src/androidTest/java/io/agora/ktvkit/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package io.agora.agoramessagetubekit;
+package io.agora.ktvkit;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("io.agora.agoramessagetubekit.test", appContext.getPackageName());
+        assertEquals("io.agora.ktvkit", appContext.getPackageName());
     }
 }
