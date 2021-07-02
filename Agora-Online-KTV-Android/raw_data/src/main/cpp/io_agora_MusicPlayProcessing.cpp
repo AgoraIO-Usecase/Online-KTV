e1b3f48ed8a6ddf2ab36b8bac6894201d5c4a547
chenhengfei <chenhengfei@agora.io>
2021-07-02 21:46:08 +0800

    c++

---
+++ Agora-Online-KTV-Android/raw_data/src/main/cpp/io_agora_MusicPlayProcessing.cpp
@@ -0,0 +1,32 @@
+#include <jni.h>
+#include <android/log.h>
+#include <cstring>
+#include "include/IAgoraMediaEngine.h"
+
+#include "include/IAgoraRtcEngine.h"
+#include <string.h>
+#include "io_agora_MusicPlayProcessing.h"
+#include "include/VMUtil.h"
+
+#include <map>
+
+using namespace std;
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+JNIEXPORT void JNICALL Java_io_agora_MusicPlayProcessing_start(JNIEnv *, jclass) {
+
+}
+
+JNIEXPORT void JNICALL Java_io_agora_MusicPlayProcessing_stop(JNIEnv *, jclass) {
+
+}
+
+JNIEXPORT void JNICALL Java_io_agora_MusicPlayProcessing_change(JNIEnv *, jclass, jint index) {
+
+}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ Agora-Online-KTV-Android/raw_data/src/main/java/io/agora/MusicPlayProcessing.java
@@ -0,0 +1,13 @@
+package io.agora;
+
+/**
+ * @author chenhengfei(Aslanchen)
+ * @date 2021/7/2
+ */
+public class MusicPlayProcessing {
+    public static native void start();
+
+    public static native void stop();
+
+    public static native void change(int index);
+}
