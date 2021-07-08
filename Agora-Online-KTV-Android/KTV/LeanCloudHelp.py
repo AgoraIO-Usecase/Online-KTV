0cb0d2ca737fc804b585dba93cb8db18e06447f7
chenhengfei <chenhengfei@agora.io>
2021-07-08 11:19:46 +0800

    动画切换

--- Agora-Online-KTV-Android/KTV/LeanCloudHelp.py
+++ Agora-Online-KTV-Android/KTV/LeanCloudHelp.py
@@ -2,8 +2,8 @@
 
 import leancloud
 
-appid = 'HlhdxEP2mt0agmaDECdzHRrP-9Nh9j0Va'
-appkey = 'cuBS7GYWcGvOFfV9oBVXj5lf'
+appid = ''
+appkey = ''
 
 isInit = False
 
@@ -34,33 +34,6 @@ def createTable():
     user.set('avatar', '1')
     user.save()
 
-    # Room
-    ROOM = leancloud.Object.extend('ROOM_MARRY')
-    room = ROOM()
-    room.set('anchorId', user)
-    room.set('channelName', "TestName")
-    room.save()
-
-    # Member
-    MEMBER = leancloud.Object.extend('MEMBER_MARRY')
-    member = MEMBER()
-    member.set('isMuted', 0)
-    member.set('isSelfMuted', 0)
-    member.set('role', 1)
-    member.set('roomId', room)
-    member.set('streamId', 10000)
-    member.set("userId", user)
-    member.save()
-
-    # Action
-    ACTION = leancloud.Object.extend('ACTION_MARRY')
-    action = ACTION()
-    action.set('action', 1)
-    action.set('memberId', member)
-    action.set('roomId', room)
-    action.set('status', 1)
-    action.save()
-
     # AgoraRoom
     AgoraRoom = leancloud.Object.extend('AGORA_ROOM')
     mAgoraRoom = AgoraRoom()
@@ -80,11 +53,18 @@ def createTable():
     mAgoraMember.set('isSelfMuted', 0)
     mAgoraMember.save()
 
+    MusicKTV = leancloud.Object.extend('MUSIC_KTV')
+    mMusicKTV = MusicKTV()
+    mMusicKTV.set('name', 'Test')
+    mMusicKTV.set('userId', 'Test')
+    mMusicKTV.set('roomId', mAgoraRoom)
+    mMusicKTV.set('musicId', 'Test')
+    mMusicKTV.save()
+
     user.destroy()
-    room.destroy()
-    member.destroy()
-    action.destroy()
     mAgoraRoom.destroy()
+    mAgoraMember.destroy()
+    mMusicKTV.destroy()
     print("Done!!")
 
 
