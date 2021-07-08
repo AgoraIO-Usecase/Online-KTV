57d1340bc659c327586858245927ae86160ba4ca
chenhengfei <chenhengfei@agora.io>
2021-07-08 15:54:23 +0800

    合唱隐藏

--- Agora-Online-KTV-Android/KTV/src/main/java/io/agora/ktv/view/RoomActivity.java
+++ Agora-Online-KTV-Android/KTV/src/main/java/io/agora/ktv/view/RoomActivity.java
@@ -577,7 +577,7 @@ public class RoomActivity extends DataBindBaseActivity<KtvActivityRoomBinding> i
         mDataBinding.ivMic.setVisibility(View.VISIBLE);
         mDataBinding.ivBackgroundPicture.setVisibility(View.VISIBLE);
         mDataBinding.llChooseSong.setVisibility(View.VISIBLE);
-        mDataBinding.ivChorus.setVisibility(View.VISIBLE);
+        mDataBinding.ivChorus.setVisibility(View.INVISIBLE);
         mDataBinding.tvNoOnSeat.setVisibility(View.GONE);
     }
 
