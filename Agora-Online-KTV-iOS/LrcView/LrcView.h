dc3a933660cfc1ff1e72403ff3e0c923d9e52b84
xianing <xianing@agora.io>
2021-08-10 12:09:09 +0800

    add missing files

---
+++ Agora-Online-KTV-iOS/LrcView/LrcView.h
@@ -0,0 +1,18 @@
+//
+//  LrcView.h
+//  LrcView
+//
+//  Created by xianing on 2021/8/7.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LrcView.
+FOUNDATION_EXPORT double LrcViewVersionNumber;
+
+//! Project version string for LrcView.
+FOUNDATION_EXPORT const unsigned char LrcViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LrcView/PublicHeader.h>
+
+
