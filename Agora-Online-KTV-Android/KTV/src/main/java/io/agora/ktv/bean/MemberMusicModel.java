cbcf8d78cca91a5a610cc09b1b39b086f97158dd
chenhengfei <chenhengfei@agora.io>
2021-08-13 15:36:09 +0800

    歌曲图片，以及歌手名称

--- Agora-Online-KTV-Android/KTV/src/main/java/io/agora/ktv/bean/MemberMusicModel.java
+++ Agora-Online-KTV-Android/KTV/src/main/java/io/agora/ktv/bean/MemberMusicModel.java
@@ -9,6 +9,7 @@ import com.agora.data.sync.DocumentReference;
 import com.agora.data.sync.SyncManager;
 
 import java.io.File;
+import java.io.Serializable;
 import java.util.HashMap;
 
 /**
@@ -19,12 +20,14 @@ public class MemberMusicModel implements Parcelable {
     public static final String TABLE_NAME = "MUSIC_KTV";
 
     public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_SINGER = "singer";
+    public static final String COLUMN_POSTER = "poster";
     public static final String COLUMN_USERID = "userId";
     public static final String COLUMN_ROOMID = "roomId";
     public static final String COLUMN_MUSICID = "musicId";
     public static final String COLUMN_CREATE = "createdAt";
 
-    public enum Type {
+    public enum Type implements Serializable {
         Default, MiGu;
     }
 
@@ -51,15 +54,18 @@ public class MemberMusicModel implements Parcelable {
     public MemberMusicModel(MusicModel data) {
         this.name = data.getName();
         this.musicId = data.getMusicId();
+        this.singer = data.getSinger();
+        this.poster = data.getPoster();
     }
 
     protected MemberMusicModel(Parcel in) {
         id = in.readString();
         name = in.readString();
-        userId = in.readString();
         roomId = in.readParcelable(AgoraRoom.class.getClassLoader());
         musicId = in.readString();
         song = in.readString();
+        singer = in.readString();
+        poster = in.readString();
         lrc = in.readString();
         fileMusic = (File) in.readSerializable();
         fileLrc = (File) in.readSerializable();
@@ -71,14 +77,16 @@ public class MemberMusicModel implements Parcelable {
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(id);
         dest.writeString(name);
-        dest.writeString(userId);
         dest.writeParcelable(roomId, flags);
         dest.writeString(musicId);
         dest.writeString(song);
+        dest.writeString(singer);
+        dest.writeString(poster);
         dest.writeString(lrc);
         dest.writeSerializable(fileMusic);
         dest.writeSerializable(fileLrc);
         dest.writeSerializable(type);
+        dest.writeString(userId);
     }
 
     @Override
@@ -98,11 +106,6 @@ public class MemberMusicModel implements Parcelable {
         }
     };
 
-    public void setLocalFile(File fileMusic, File fileLrc) {
-        this.fileMusic = fileMusic;
-        this.fileLrc = fileLrc;
-    }
-
     public HashMap<String, Object> toHashMap() {
         DocumentReference drRoom = SyncManager.Instance()
                 .collection(AgoraRoom.TABLE_NAME)
@@ -110,92 +113,14 @@ public class MemberMusicModel implements Parcelable {
 
         HashMap<String, Object> datas = new HashMap<>();
         datas.put(COLUMN_NAME, name);
-        datas.put(COLUMN_USERID, userId);
+        datas.put(COLUMN_SINGER, singer);
+        datas.put(COLUMN_POSTER, poster);
         datas.put(COLUMN_ROOMID, drRoom);
         datas.put(COLUMN_MUSICID, musicId);
+        datas.put(COLUMN_USERID, userId);
         return datas;
     }
 
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public AgoraRoom getRoomId() {
-        return roomId;
-    }
-
-    public void setRoomId(AgoraRoom roomId) {
-        this.roomId = roomId;
-    }
-
-    public String getSong() {
-        return song;
-    }
-
-    public void setSong(String song) {
-        this.song = song;
-    }
-
-    public String getLrc() {
-        return lrc;
-    }
-
-    public void setLrc(String lrc) {
-        this.lrc = lrc;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getMusicId() {
-        return musicId;
-    }
-
-    public void setMusicId(String musicId) {
-        this.musicId = musicId;
-    }
-
-    public File getFileMusic() {
-        return fileMusic;
-    }
-
-    public void setFileMusic(File fileMusic) {
-        this.fileMusic = fileMusic;
-    }
-
-    public File getFileLrc() {
-        return fileLrc;
-    }
-
-    public void setFileLrc(File fileLrc) {
-        this.fileLrc = fileLrc;
-    }
-
-    public Type getType() {
-        return type;
-    }
-
-    public void setType(Type type) {
-        this.type = type;
-    }
 
     @Override
     public boolean equals(Object o) {
@@ -221,10 +146,16 @@ public class MemberMusicModel implements Parcelable {
         return "MemberMusicModel{" +
                 "id='" + id + '\'' +
                 ", name='" + name + '\'' +
+                ", singer='" + singer + '\'' +
+                ", poster='" + poster + '\'' +
                 ", userId='" + userId + '\'' +
-                ", roomId='" + roomId + '\'' +
+                ", roomId=" + roomId +
                 ", musicId='" + musicId + '\'' +
+                ", song='" + song + '\'' +
+                ", lrc='" + lrc + '\'' +
+                ", fileMusic=" + fileMusic +
+                ", fileLrc=" + fileLrc +
+                ", type=" + type +
                 '}';
     }
-
 }
